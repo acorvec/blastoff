eafa840812b4ca0709b7b1a93f8af654c282d2ad
Andrew Corvec <andrew@corvec.org>
2026-03-05 17:30:02 -0600

    removed emscripten build support permanently. this library is the reincarnation of the devil in software form

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+#!/bin/bash
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
 make -j 12
