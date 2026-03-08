ca6273a5b94615a729a0345998312c465b53e50e
Andrew Corvec <andrew@corvec.org>
2026-03-07 20:52:25 -0600

    almost got emscripten working. ignore the swear words

--- source/BlastOffpp.cpp
+++ source/BlastOffpp.cpp
@@ -1,5 +1,7 @@
 #include "Program.h"
 
+#ifndef __EMSCRIPTEN__
+
 int main()
 {
     BlastOff::Program program;
@@ -8,3 +10,24 @@ int main()
 
     return 0;
 }
+
+#else
+
+#include <emscripten.h>
+
+BlastOff::Program program;
+
+static void mainloop()
+{
+    program.RunLoopIteration();
+    if (!program.IsRunning())
+        emscripten_cancel_main_loop();
+}
+
+int main()
+{
+    emscripten_set_main_loop(mainloop, 0, 1);
+    return 0;
+}
+
+#endif
