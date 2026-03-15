26eea3eb12c00b787fcaa626194b64868684be86
Andrew Corvec <andrew@corvec.org>
2026-03-15 12:46:03 -0500

    fixed some random broken stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug 
 make -j 12
