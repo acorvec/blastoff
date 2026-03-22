d4f3ad31079c0659e37b8d2b8fda140481bb226c
Andrew Corvec <andrew@corvec.org>
2026-03-21 20:05:25 -0500

    removed curses

--- embuild.bat
+++ embuild.bat
@@ -1,5 +1,2 @@
-:: fuck emscripten. all my homies hate emscripten
-:: (seriously, what the fuck is wrong with this god-awful package?)
-
 emcmake cmake -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
 emmake make -j 12
--- embuild.sh
+++ embuild.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# fuck emscripten. all my homies hate emscripten
-# (seriously, what the fuck is wrong with this god-awful package?)
-
 emcmake cmake -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
 emmake make -j 12
