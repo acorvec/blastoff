6a483d393909010a3a6ecf9ddbeed4dc85dd99f5
Andrew Corvec <andrew@corvec.org>
2026-03-23 15:50:05 -0500

    updated gitignore

--- CMakeFiles/fc-tmp/rapidjson/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for rapidjson")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/developer/source/repos/mine/blastoff/CMakeFiles/fc-tmp/rapidjson/rapidjson-gitclone.cmake")
-
-endblock()
--- CMakeFiles/fc-tmp/rapidjson/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for rapidjson")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/developer/source/repos/mine/blastoff/CMakeFiles/fc-tmp/rapidjson/rapidjson-gitupdate.cmake")
-
-endblock()
