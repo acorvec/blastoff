713e9f39d3a2dc0981fb90a8cf4f9497aa9fb54f
Andrew Corvec <andrew@corvec.org>
2026-03-23 18:12:40 -0500

    switched to Ninja build system (make is now broken with Raylib)

--- build/embuild.sh
+++ build/embuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
-emmake make -j 12
+cp -r ../resource/ ./
+#emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
+emcmake cmake .. -G Ninja -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
+#emmake make -j 12
+ninja -j12
---
+++ embuild.bat
@@ -0,0 +1,2 @@
+emcmake cmake -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug --toolchain $EMROOT/cmake/Modules/Platform/Emscripten.cmake
+emmake make -j 12
