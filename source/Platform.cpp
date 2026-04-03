458df62688bed94f1b7558a4a28b381704141e3f
Andrew Corvec <andrew@corvec.org>
2026-04-03 12:57:09 -0500

    cleaned up dead code

--- source/Platform.cpp
+++ source/Platform.cpp
@@ -94,7 +94,6 @@ namespace BlastOff
 	namespace Emscripten
 	{
         std::function<void(int, int)> setWindowSize;
-        std::function<void()> deinitialize;
         float aspectRatio = 0;
 
         CursorPosition mostRecentCursorPosition = { 0, 0 };
